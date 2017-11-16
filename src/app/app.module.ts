2e65b0f3443d5dfd6d8c71f2998266988d53d18d
Saban Uenlue - netTrek <us@netTrek.de>
2017-11-16 14:21:03 +0100

    first components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MyModuleModule } from './my-module/my-module.module';
 
 @NgModule({
   declarations: [
@@ -10,6 +11,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    MyModuleModule,
     AppRoutingModule
   ],
   providers: [],
