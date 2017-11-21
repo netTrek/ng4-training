ad54de23edaf2d3694e000597fc1c412fb44e3bb
Saban Uenlue - netTrek <us@netTrek.de>
2017-11-21 10:03:27 +0100

    the perfect doHover (without css)

--- src/app/utils/do-hover.directive.ts
+++ src/app/utils/do-hover.directive.ts
@@ -1,30 +1,40 @@
-import { Directive, HostBinding, HostListener, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 
-@Directive({
+@Directive ( {
   selector: '[gfnDoHover]'
-})
-export class DoHoverDirective implements OnChanges {
+} )
+export class DoHoverDirective implements OnChanges, OnInit {
 
-  @Input()
-  @HostBinding ('style.color')
-  gfnDoHover = 'red';
+  @Input ()
+  @HostBinding ( 'style.color' )
+  gfnDoHover: string;
 
   private _useColor: string;
+  private _initColor: string | null = null;
 
-  constructor( ) {}
+  constructor ( elem: ElementRef ) {
+    if ( elem.nativeElement.style.color !== '' ) {
+      this._initColor = elem.nativeElement.style.color;
+    }
+  }
 
-  @HostListener ('mouseenter')
+  ngOnInit (): void {
+    this.gfnDoHover = this._initColor;
+  }
+
+  @HostListener ( 'mouseenter' )
   onEnter () {
     this.gfnDoHover = this._useColor;
   }
-  @HostListener ('mouseleave')
+
+  @HostListener ( 'mouseleave' )
   onLeave () {
-    this.gfnDoHover = null;
+    this.gfnDoHover = this._initColor;
   }
 
   ngOnChanges ( changes: SimpleChanges ): void {
-    if ( !!changes.gfnDoHover && changes.gfnDoHover.currentValue === '' ) {
-      this._useColor = this.gfnDoHover = 'red';
+    if ( ! ! changes.gfnDoHover && changes.gfnDoHover.currentValue === '' ) {
+      this._useColor = 'red';
     } else {
       this._useColor = this.gfnDoHover;
     }
