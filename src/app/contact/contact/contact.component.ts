41a9073a06e7ed0564f625af92d50616e379692d
Saban Uenlue - netTrek <us@netTrek.de>
2017-11-22 12:15:06 +0100

    navigate

--- src/app/contact/contact/contact.component.ts
+++ src/app/contact/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'gfn-contact',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
-  constructor() { }
+  constructor( private $router: Router ) { }
 
   ngOnInit() {
   }
 
+  goHome() {
+    this.$router.navigate( [ 'home' ] );
+  }
 }
