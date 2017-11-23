ecba1c531231874a8679854f59395240f38bf87b
Saban Uenlue - netTrek <us@netTrek.de>
2017-11-23 08:45:46 +0100

    play for testing

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,42 +1,55 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 import { UtilsModule } from './utils/utils.module';
 import { UserModule } from './user/user.module';
 import { HttpClientModule } from '@angular/common/http';
 import { By } from '@angular/platform-browser';
-describe('AppComponent', () => {
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      imports: [
+import { HandelErrorModule } from './handel-error/handel-error.module';
+import { DebugElement } from '@angular/core';
+
+describe ( 'AppComponent', () => {
+
+  let fixture: ComponentFixture<AppComponent>;
+  let app: AppComponent;
+  let h1DebugElement: DebugElement;
+  let h1: HTMLElement;
+
+  beforeEach ( async ( () => {
+    TestBed.configureTestingModule ( {
+      imports     : [
         RouterTestingModule,
         UtilsModule,
-        UserModule
+        UserModule,
+        HandelErrorModule
       ],
       declarations: [
         AppComponent
-      ],
-    }).compileComponents();
-  }));
-  it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  }));
-  it(`should have as title 'gfn'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('gfn');
-  }));
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect( fixture.debugElement.nativeElement
-                   .querySelector('h1').textContent)
-      .toContain('Welcome to gfn!');
-    expect(fixture.debugElement
-                  .query(By.css('h1')).nativeElement.textContent)
-      .toContain('Welcome to gfn!');
-  }));
-});
+      ]
+    } )
+           .compileComponents ();
+  } ) );
+
+  beforeEach ( () => {
+    fixture = TestBed.createComponent ( AppComponent );
+    app     = fixture.debugElement.componentInstance;
+
+    // query for the title <h1> by CSS element selector
+    h1DebugElement = fixture.debugElement.query ( By.css ( 'h1' ) );
+    h1 = h1DebugElement.nativeElement;
+  } );
+
+  it ( 'should create the app', () => {
+    expect ( app )
+      .toBeTruthy ();
+  } );
+  it ( `should have as title 'gfn'`, () => {
+    expect ( app.title )
+      .toEqual ( 'gfn' );
+  } );
+  it ( 'should render title in a h1 tag', () => {
+    fixture.detectChanges ();
+    expect ( h1.textContent )
+      .toContain ( 'Welcome to gfn!' );
+  } );
+} );
--- src/app/user/user-resolve.service.spec.ts
+++ src/app/user/user-resolve.service.spec.ts
@@ -2,14 +2,17 @@ import { TestBed, inject } from '@angular/core/testing';
 
 import { UserResolveService } from './user-resolve.service';
 
-describe('UserResolveService', () => {
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [UserResolveService]
-    });
-  });
+describe ( 'UserResolveService', () => {
+  beforeEach ( () => {
+    TestBed.configureTestingModule ( {
+      providers: [ UserResolveService ]
+    } );
+  } );
 
-  it('should be created', inject([UserResolveService], (service: UserResolveService) => {
-    expect(service).toBeTruthy();
-  }));
-});
+  it ( 'should be created',
+    inject ( [ UserResolveService ],
+      ( service: UserResolveService ) => {
+        expect ( service )
+          .toBeTruthy ();
+      } ) );
+} );
